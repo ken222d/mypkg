89392102537e85a2ea2c099e8880d2fc4e075ed8
Kenta Ishizeki <a.w.g.d0201@icloud.com>
2024-11-24 13:11:41 +0900

    remake a command

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,17 +1,18 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from person_msgs.msg import Person
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
+pub = node.create_publisher(Person, "person", 10)
 n = 0
 
 
 def cb():
     global n
-    msg = Int16()
-    msg.data = n
+    msg = Person()
+    msg.name = "石関建大"
+    msg.age = n
     pub.publish(msg)
     n += 1
 
@@ -19,4 +20,3 @@ def cb():
 def main():
     node.create_timer(0.5, cb)
     rclpy.spin(node)
-
